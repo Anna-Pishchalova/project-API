0cbd9f485bb4f21368f555667ed6219299dfbb62
Ann <annapisalova@gmail.com>
2026-03-19 13:43:58 +0700

    Create and update folder Models

---
+++ Models/Specialty.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace app.Models
+{
+    [Table("specialties")]
+    public class Specialty
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("name")]
+        [Required]
+        public string Name { get; set; } = null!;
+    }
+}
